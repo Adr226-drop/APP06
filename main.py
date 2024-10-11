22c5729adf91a3983cebe78fbd159b14229d66e6
Adr226-drop <pedrozaromeroadriannicolas@cetis50cdmx.com>
2024-10-10 20:40:37 -0600

    versión1.2

--- main.py
+++ main.py
@@ -1,45 +1,76 @@
 import flet as ft
-#se importa libreria random
 import random
 
-def main(page: ft.Page):
-    #se crea las variables locales
-    global numero_secreto,entrada_numero,text_resultado,boton_adivinar
+#funcion para adivinar el numero
+def verificar_adivinanza(e,page):
+    adivinanza_usuario=int(entrada_numero.value)
     
+    if adivinanza_usuario==numero_Secreto:
+        texto_resultado.value="¡Felicidades! Adivinaste el numero secreto"
+        boton_adivinar.disabled=True
+        page.add(ft.Audio(src="Victoria.mp3",autoplay=True))
+    elif adivinanza_usuario < numero_Secreto:
+        texto_resultado.value="¡Fallaste! El numero secreto es mayor"
+        page.add(ft.Audio(src="Boing.mp3",autoplay=True))
+    else:
+        texto_resultado.value="¡Fallaste! El numero secreto es menor"
+        page.add(ft.Audio(src="Boing.mp3",autoplay=True))
+        
+    entrada_numero.value=""
+    page.update()
+        
+
+
+
+
+
+
+
+
+
+
+#Función principal
+def main(page: ft.Page):
+    #Variables globales
+    global numero_Secreto,entrada_numero,texto_resultado,boton_adivinar
     
     page.title = "Adivina el número"
     
-    numero_secreto = random.randint(1,100)
+    #Generar un número aleatorio
+    numero_Secreto = random.randint(1,100)
     
-    titulo=ft.Text("Adivina el número",size=25,color="white")
-    entrada_numero=ft.TextField(label="Tu adivinanza entre 1 y 100",width=300)
+    #Crear los elementos de la interfaz
+    titulo=ft.Text("Adivina el número secreto entre 1 y 100",size= 20,color="white")
+    entrada_numero=ft.TextField(label="Tu Adivinanza",width=150)
     boton_adivinar=ft.ElevatedButton("Adivinar")
-    text_resultado=ft.Text("",color="white")
-
-    #se crea el contenedor de la interfaz grafica
+    texto_resultado=ft.Text("",color="white")
+    
     contenedor_principal=ft.Container(
         content=ft.Column(
             controls=[
                 titulo,
                 entrada_numero,
                 boton_adivinar,
-                text_resultado,
+                texto_resultado,
                 ft.Image(
                     src="https://i.ibb.co/Gxgryg9/laser.gif",
                     fit=ft.ImageFit.COVER,
                     width=350,
-                    height=300
-                    
+                    height=200
                 )
             ],alignment="CENTER",
             horizontal_alignment="CENTER",
             spacing=20
-            
         ),
         bgcolor="black",
-        width=page.window.width,
+        width=page.window.width,    
         height=page.window.height,
         padding=20
+            
+        
     )
+    page.add(contenedor_principal)
+  
+
 
 ft.app(main)
